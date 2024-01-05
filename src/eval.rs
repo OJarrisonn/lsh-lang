4721211ea55c9d2488588087a25279371640fdd4
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-04 22:48:34 -0300

    Changed errors to match the new error definition Need to add the proper place and point info

--- src/eval.rs
+++ src/eval.rs
@@ -1,12 +1,12 @@
 use pest::iterators::Pairs;
 
-use crate::{parse::{Rule, expression::{Expression, Call, Function}}, error::LSHError};
+use crate::{parse::{Rule, expression::{Expression, Call, Function}}, error::{LSHRuntimeErrorStack, LSHRuntimeErrorKind}};
 
 use self::symbol_table::SymbolTable;
 
 pub mod symbol_table;
 
-pub fn eval(table: &mut SymbolTable, expr: Expression) -> Result<Expression, LSHError> {
+pub fn eval(table: &mut SymbolTable, expr: Expression) -> Result<Expression, LSHRuntimeErrorStack> {
     match expr {
         Expression::List(list) => {
             let mut results: Vec<_> = list.into_iter()
@@ -18,15 +18,14 @@ pub fn eval(table: &mut SymbolTable, expr: Expression) -> Result<Expression, LSH
                 None => Ok(Expression::Nil)
             }
         },
+
         Expression::DataList(list) => {
-            let mut failure: Option<LSHError> = None;
+            let mut failure: Option<LSHRuntimeErrorStack> = None;
             let results: Vec<Expression> = list.into_iter()
                 .map(|expr| eval(table, expr))
                 .map(|res| match res {
                     Ok(expr) => expr,
                     Err(e) => {
-                        eprintln!("{e}");
-                        
                         if failure.is_none() {
                             failure = Some(e);
                         }
@@ -37,10 +36,13 @@ pub fn eval(table: &mut SymbolTable, expr: Expression) -> Result<Expression, LSH
                 .collect();
 
             match failure {
-                Some(e) => Err(e),
+                Some(e) => Err(
+                    LSHRuntimeErrorStack::create_stream("u", (0,0), e)
+                ),
                 None => Ok(Expression::DataList(results))
             }
         },
+
         Expression::Call(call) => {
             match call {
                 Call::FunctionCall(symbol, args) => {
@@ -48,16 +50,26 @@ pub fn eval(table: &mut SymbolTable, expr: Expression) -> Result<Expression, LSH
                     let args = eval(table, Expression::DataList(args));
 
                     if let Err(e) = args {
-                        return Err(e);
+                        return Err(
+                            LSHRuntimeErrorStack::create_stream("u", (0,0), e)
+                        );
                     }
 
                     let args = args.unwrap().into();
                     
                     match func {
-                        Err(e) => Err(e),
-                        Ok(func) => match func {
+                        Err(e) => Err(
+                            LSHRuntimeErrorStack::create_stream("u", (0,0), e)
+                        ),
+                        Ok(expr) => match expr {
                             Expression::Function(func) => func.exec(table, args),
-                            _ => Err(LSHError::not_a_function(func))
+                            _ => Err(LSHRuntimeErrorStack
+                                ::create_source(
+                                    "u", 
+                                    (0,0), 
+                                    LSHRuntimeErrorKind::NotCallable(&expr))
+                                .append_note(String::from("A macro was expected"))
+                            )
                         }
                     }
 
@@ -66,10 +78,18 @@ pub fn eval(table: &mut SymbolTable, expr: Expression) -> Result<Expression, LSH
                     let mac = eval(table, Expression::Symbol(symbol));
 
                     match mac {
-                        Err(e) => Err(e),
-                        Ok(mac) => match mac {
+                        Err(e) => Err(
+                            LSHRuntimeErrorStack::create_stream("u", (0,0), e)
+                        ),
+                        Ok(expr) => match expr {
                             Expression::Macro(mac) => mac.exec(table, args),
-                            _ => Err(LSHError::not_a_macro(mac))
+                            _ => Err(LSHRuntimeErrorStack
+                                ::create_source(
+                                    "u", 
+                                    (0,0),
+                                    LSHRuntimeErrorKind::NotCallable(&expr))
+                                .append_note(String::from("A macro was expected"))
+                            )
                         },
                     }
                 },
@@ -78,7 +98,13 @@ pub fn eval(table: &mut SymbolTable, expr: Expression) -> Result<Expression, LSH
         Expression::Symbol(symbol) => {
             match table.get(&symbol) {
                 Some(value) => Ok(value),
-                None => Err(LSHError::undefined_symbol(symbol)),
+                None => Err(LSHRuntimeErrorStack
+                    ::create_source(
+                        "u", 
+                        (0,0), 
+                        LSHRuntimeErrorKind::UndefinedIdentifier(&symbol)
+                    )
+                ),
             }
         },
         expr => Ok(expr)
